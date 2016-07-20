8a56ba59a82b313cd31320f414bf1bf0bb1e3bc0
ViniciusArnhold <viniciuspegoriniarnhold@gmail.com>
2016-07-19 22:00:41 -0300

    Added Preferences API support

--- src/main/java/me/vinicius/correios/tracker/Controller.java
+++ src/main/java/me/vinicius/correios/tracker/Controller.java
@@ -16,11 +16,11 @@ import javafx.util.Duration;
 import me.vinicius.correios.api.Event;
 import me.vinicius.correios.api.Rastreamento;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
-import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -37,7 +37,7 @@ class Controller {
     private URL location;
 
     @FXML
-    private ListView<String> codeList;
+    private ListView<String> codeListView;
 
     @FXML
     private VBox vBox;
@@ -55,9 +55,11 @@ class Controller {
     private Button bDelete;
 
     @FXML
-    private ListView<String> eventList;
+    private ListView<String> eventListView;
 
     private Stage primaryStage;
+
+    private ObservableList<String> codeList;
 
     void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -65,30 +67,25 @@ class Controller {
 
     @FXML
     void initialize() {
-        assert codeList != null : "fx:id=\"codeList\" was not injected: check your FXML file 'interface.fxml'.";
+        assert codeListView != null : "fx:id=\"codeListView\" was not injected: check your FXML file 'interface.fxml'.";
         assert vBox != null : "fx:id=\"vBox\" was not injected: check your FXML file 'interface.fxml'.";
         assert hBox != null : "fx:id=\"hBox\" was not injected: check your FXML file 'interface.fxml'.";
         assert bAdd != null : "fx:id=\"bAdd\" was not injected: check your FXML file 'interface.fxml'.";
         assert bView != null : "fx:id=\"bEdit\" was not injected: check your FXML file 'interface.fxml'.";
         assert bDelete != null : "fx:id=\"bDelete\" was not injected: check your FXML file 'interface.fxml'.";
-        assert eventList != null : "fx:id=\"eventList\" was not injected: check your FXML file 'interface.fxml'.";
+        assert eventListView != null : "fx:id=\"eventListView\" was not injected: check your FXML file 'interface.fxml'.";
         assert comboBox != null : "fx:id=\"comboBox\" was not injected: check your FXML file 'interface.fxml'.";
-
-
         //BEGIN controls option settings
         //ComboBox
         comboBox.setEditable(true);
-
-
         //Lists
-        codeList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        codeList.setEditable(false);
-
-        eventList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        eventList.setEditable(false);
-        eventList.setOnKeyPressed(event -> {
+        codeListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        codeListView.setEditable(false);
+        eventListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        eventListView.setEditable(false);
+        eventListView.setOnKeyPressed(event -> {
             if (event.isControlDown() && Objects.equals(event.getCode().getName(), "C")) {
-                ObservableList rowList = (ObservableList) eventList.getSelectionModel().getSelectedItems();
+                ObservableList rowList = (ObservableList) eventListView.getSelectionModel().getSelectedItems();
                 StringBuilder clipboardString = new StringBuilder();
                 for (Object row : rowList) {
                     if (row == null) {
@@ -102,28 +99,25 @@ class Controller {
                 Clipboard.getSystemClipboard().setContent(content);
             }
         });
-
         //Buttons
         bDelete.setStyle("-fx-base: red;");
-
         //END controls option settings
 
         /* BEGIN Buttons Action Handlers */
         bAdd.setOnAction(event -> {
-            System.out.println(comboBox.getValue());
-            if (comboBox.getValue() != null) {
-                String code = comboBox.getValue().toString().toUpperCase();
-                Matcher m = Pattern.compile("[A-Z]{2}\\d{9}[A-Z]*").matcher(code); //Quick Match
+            if (comboBox.getSelectionModel().getSelectedItem() != null) {
+                String code = comboBox.getSelectionModel().getSelectedItem().toString().toUpperCase();
+                Matcher m = Pattern.compile("[A-Z]{2}\\d{9}[A-Z]{1,3}$").matcher(code); //Quick Match
                 if (m.find()) {
-                    if (!codeList.getItems().contains(code)) {
-                        codeList.getItems().add(0, code);
+                    if (!codeListView.getItems().contains(code)) {
+                        codeListView.getItems().add(0, code);
                         comboBox.setStyle("-fx-focus-color: #338FFF");
                         comboBox.requestFocus();
                     } else {
-                        codeList.requestFocus();
-                        codeList.scrollTo(code);
-                        codeList.getSelectionModel().select(code);
-                        codeList.getFocusModel().focus(codeList.getItems().indexOf(code));
+                        codeListView.requestFocus();
+                        codeListView.scrollTo(code);
+                        codeListView.getSelectionModel().select(code);
+                        codeListView.getFocusModel().focus(codeListView.getItems().indexOf(code));
                     }
                 } else {
                     comboBox.setStyle("-fx-focus-color: red");
@@ -133,44 +127,37 @@ class Controller {
                 comboBox.setStyle("-fx-focus-color: red");
                 comboBox.requestFocus();
             }
-            System.out.println("Clicked on Added");//Debug
         });
         bView.setOnAction(event -> {
-            System.out.println("Clicked on View");//Debug
-            if (codeList.getItems().size() != 0 && codeList.getSelectionModel().getSelectedItem() != null) {
-
-                eventList.getItems().clear();
-                Rastreamento res = new Rastreamento(codeList.getSelectionModel().getSelectedItem());
+            if (codeListView.getItems().size() != 0 && codeListView.getSelectionModel().getSelectedItem() != null) {
+                eventListView.getItems().clear();
+                Rastreamento res = new Rastreamento(codeListView.getSelectionModel().getSelectedItem());
                 try {
                     res.track();
                 } catch (IOException e) {
-                    eventList.getItems().add("Connection Timed Out: Check your connection.");
+                    eventListView.getItems().add("Connection Timed Out: Check your connection.");
                 } finally {
                     Event eventsReversed[] = res.getEvents() != null ? res.getEvents() : null;
                     if (eventsReversed != null) {
                         Event events[] = new Event[eventsReversed.length];
-
                         //Sort the array in chronological order
                         if (events.length == 0) {
-                            eventList.getItems().add("Package not found, check you code");
+                            eventListView.getItems().add("Package not found, check you code");
                         } else {
                             for (int i = 0; i < eventsReversed.length; i++) {
                                 events[i] = eventsReversed[eventsReversed.length - i - 1];
                             }
                             for (Event e : events) {
-                                eventList.getItems().add(0, "");
-                                eventList.getItems().add(0, e.getMovement());
-                                eventList.getItems().add(0, "Estava em: " + e.getLocal());
-                                eventList.getItems().add(0, e.getAction() + " em " + e.getData());
+                                eventListView.getItems().add(0, "");
+                                eventListView.getItems().add(0, e.getMovement());
+                                eventListView.getItems().add(0, "Estava em: " + e.getLocal());
+                                eventListView.getItems().add(0, e.getAction() + " em " + e.getData());
                             }
-
-                            //Notification -  For Testing - Debug
-                            String title = "Rastreamento de: " + codeList.getSelectionModel().getSelectedItem() + "\n" +
+                            //Notification -  For Testing
+                            String title = "Rastreamento de: " + codeListView.getSelectionModel().getSelectedItem() + "\n" +
                                     eventsReversed[0].getAction() + " em " + eventsReversed[0].getData();
                             String message = eventsReversed[0].getMovement();
                             Notifications notification = Notifications.SUCCESS;
-
-
                             TrayNotification tray = new TrayNotification(title, message, notification);
                             tray.setImage(new Image("http://globalestudio.com.br/loja/image/cache/catalog/PRODUTOS/modulosmodulo-de-frete-correios-opencart-54-746x746.jpg"));
                             tray.setAnimation(Animations.FADE);
@@ -183,75 +170,50 @@ class Controller {
             }
         });
         bDelete.setOnAction(event -> {
-            System.out.println("Clicked on Delete"); //Debug
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Confirmation Dialog");
-            alert.setHeaderText("This will delete the value: " + codeList.getSelectionModel().getSelectedItem());
+            alert.setHeaderText("This will delete the value: " + codeListView.getSelectionModel().getSelectedItem());
             alert.setContentText("Are you sure?");
-
             alert.showAndWait();
             if (alert.getResult() == ButtonType.OK) {
-                codeList.getItems().remove(codeList.getSelectionModel().getSelectedItem());
+                codeListView.getItems().remove(codeListView.getSelectionModel().getSelectedItem());
             }
 
         });
         /* END Buttons Action Handlers */
-
         //ComboBox Action Events
         comboBox.setOnAction(event -> {
-
         });
-
         //Primary Stage events
         //Save Properties
         primaryStage.setOnCloseRequest(event -> {
-            System.out.println("Primary Stage is closing");//Debug
-            try {
-                FileOutputStream fos = new FileOutputStream("c:/myconfig.property");
-                Properties prop = new Properties();
+            Preferences pref = Preferences.userNodeForPackage(getClass());
+            pref.putBoolean("notFirstRun", true);
+            pref.put("codeList", codeListView.getItems().toString());
 
-                int index = 0;
-                for (String s : codeList.getItems()) {
 
-                    prop.put("me.viniciusarnhold.correios.tracker.codeList", codeList.getItems().get(0).toString());
-                    prop.store(fos, "List of codes");
-                    fos.flush();
-
-                    fos.close();
-                }
-
-            } catch (FileNotFoundException e) {
-                eventList.getItems().add(e.getMessage());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         });
-
         //Load Properties
         primaryStage.setOnShown(event -> {
-            System.out.println("Primary Stage is Opening");
-
-            InputStream input = null;
-            Properties prop = new Properties();
-
-            try {
-                input = new FileInputStream("c:/myconfig.property");
-                prop.load(input);
-                codeList.getItems().add(prop.getProperty("me.viniciusarnhold.correios.tracker.codeList"));
-
-            } catch (FileNotFoundException e) {
-                //First Run, expected
-                System.out.println("Properties not found");
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (input != null) {
-                    try {
-                        input.close();
-                    } catch (IOException e) {
-                        eventList.getItems().add(e.getMessage());
+            Preferences pref = Preferences.userNodeForPackage(getClass());
+            //try {pref.clear();} catch (BackingStoreException e) {e.printStackTrace();}//Debug
+            if (pref.getBoolean("notFirstRun", false)) { // Preferences Exists
+                String str = null;
+                str = pref.get("codeList", "");
+                if (str != null) {
+                    if (str.length() != 0) {
+                        Matcher m = Pattern.compile("[A-Z]{2}\\d{9}[A-Z]{1,3}").matcher(str); //This differs from the
+                        // first because now we will have more than one tracking code
+                        while (m.find()) {
+                            comboBox.getItems().add(0, m.group());
+                            codeListView.getItems().add(0, m.group());
+                        }
                     }
+                } else {
                 }
+
+            } else { //First Run
+                //Only set "notFirstRun" when program is legally closed, avoid errors due to crashes
             }
 
 
